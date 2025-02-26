bad6d5d92b94cb1f09fa96c3bf86a7661d52a367
geedotrar <geedotrar@gmail.com>
2025-02-26 22:07:03 +0700

    feat:add data to local storage

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { TiDeleteOutline } from "react-icons/ti";
@@ -6,22 +6,37 @@ import { FiEdit } from "react-icons/fi";
 import { RiDeleteBin6Line } from "react-icons/ri";
 
 export default function FormTodo() {
-  const [todo, setTodo] = useState([
-    {
-      id: 1,
-      item: "Learn Guitar",
-    },
-    {
-      id: 2,
-      item: "Work Out",
-    },
-    {
-      id: 3,
-      item: "Learn Code",
-    },
-  ]);
-  const [inputValue, setInputValue] = useState();
+  const [todo, setTodo] = useState(() => {
+    const savedTodos = localStorage.getItem("todos");
+    const savedTime = localStorage.getItem("todosTime");
+    const currentTime = new Date().getTime();
+
+    if (savedTodos && savedTime && currentTime - parseInt(savedTime, 10) < 3600000) {
+      return JSON.parse(savedTodos);
+    }
+    return [
+      {
+        id: 1,
+        item: "Learn Guitar",
+      },
+      {
+        id: 2,
+        item: "Work Out",
+      },
+      {
+        id: 3,
+        item: "Learn Code",
+      },
+    ];
+  });
+
+  const [inputValue, setInputValue] = useState("");
   const [editTaskId, setEditTaskId] = useState(null);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todo));
+    localStorage.setItem("todosTime", new Date().getTime().toString());
+  }, [todo]);
 
   const handleChange = (event) => {
     setInputValue(event.target.value);
@@ -30,7 +45,7 @@ export default function FormTodo() {
   const handleCreate = async (e) => {
     e.preventDefault();
     const newData = {
-      id: todo.length + 1,
+      id: todo.length > 0 ? Math.max(...todo.map(t => t.id)) + 1 : 1,
       item: inputValue,
     };
     if (inputValue) {
@@ -90,7 +105,7 @@ export default function FormTodo() {
 
         {todo.map((result) => {
           return (
-            <div className="todo-row">
+            <div className="todo-row" key={result.id}>
               {result.item}
               <div>
                 <FiEdit onClick={() => handleEditTask(result.id)} className="todo-button-update" size={30}></FiEdit>
